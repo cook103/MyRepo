a1ed4a119e1b8ef2584b8773a970f5257fb520c1
mcookjr <cook103@cougars.csusm.edu>
2023-04-23 00:00:35 -0700

    forgot to add js

--- FullStackApps/StockApp/static/main.js
+++ FullStackApps/StockApp/static/main.js
@@ -1,10 +1,12 @@
 window.onload = function(){
+
     //console.log(data);
-    
+
     let tStockContainer = $('.stock_container');
     let tCryptoContainer = $('.crypto_container');
 
     //TODO: need to split name and price into stacked divs or span
+
     for (let tELem in stock_dict["name"] && stock_dict["price"]){
         tStockContainer.append('<div class="name">' + stock_dict["name"][tELem] +
                  ": " + stock_dict["price"][tELem]+  '</div>');
@@ -14,7 +16,4 @@ window.onload = function(){
         tCryptoContainer.append('<div class="name">' + crypto_dict["name"][tELem] +
                  ": " + crypto_dict["price"][tELem]+  '</div>');
     }
-
-};
-
-//end
+}
