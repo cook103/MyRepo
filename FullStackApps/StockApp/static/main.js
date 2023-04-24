b65853869589315679f9c06728cf92509e75196a
mcookjr <cook103@cougars.csusm.edu>
2023-04-23 18:02:18 -0700

    got the data working but html is not being updated correctly

--- FullStackApps/StockApp/static/main.js
+++ FullStackApps/StockApp/static/main.js
@@ -1,19 +1,48 @@
 window.onload = function(){
 
-    //console.log(data);
-
     let tStockContainer = $('.stock_container');
     let tCryptoContainer = $('.crypto_container');
 
     //TODO: need to split name and price into stacked divs or span
-
-    for (let tELem in stock_dict["name"] && stock_dict["price"]){
-        tStockContainer.append('<div class="name">' + stock_dict["name"][tELem] +
-                 ": " + stock_dict["price"][tELem]+  '</div>');
+    /*
+    for (let tELem in stock_data["name"] && stock_data["price"]){
+        tStockContainer.append('<div class="name">' + stock_data["name"][tELem] +
+                 ": " + stock_data["price"][tELem]+  '</div>');
     }
 
-    for (let tELem in crypto_dict["name"] && crypto_dict["price"]){
-        tCryptoContainer.append('<div class="name">' + crypto_dict["name"][tELem] +
-                 ": " + crypto_dict["price"][tELem]+  '</div>');
+    for (let tELem in crypto_data["name"] && crypto_data["price"]){
+        tCryptoContainer.append('<div class="name">' + crypto_data["name"][tELem] +
+                 ": " + crypto_data["price"][tELem]+  '</div>');
     }
+    
+    */
+
+    function recurring_data(){
+        setTimeout(recurring_data,2000);
+        let x = "";
+
+        $.ajax({
+        url:'/ReccuringData',
+        type: 'GET',
+        dataType: "json",
+        success: function(data){
+                console.log(JSON.stringify(data)); 
+                
+                for (let tELem in data["crypto_name"] && data["crypto_price"]){
+                    tCryptoContainer.append('<div class="name">' + data["crypto_name"][tELem] +
+                             ": " + data["crypto_price"][tELem]+  '</div>');
+                }
+            }
+        });
+
+        /*
+
+        */
+    
+
+
+    }
+    recurring_data()
+
 }
+
--- FullStackApps/StockApp/stock_index.py
+++ FullStackApps/StockApp/stock_index.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 from flask import ( 
         Flask,
-        request,
         render_template,
-        jsonify,
-        redirect,
-        url_for,
-        session,
         json)
 
 import requests
@@ -23,50 +18,50 @@ def parse(url):
     return soup
 
 #TODO: make more adaptable, html scrapping is hard coded
-def scraped_parsed_data(data):
+def scraped_parsed_data():
     """find and store parsed html data;
     stock index: names, price, dollar change,
     and percent change"""
 
+    stock_data = parse("https://www.google.com/finance/markets/indexes") 
+    crypto_data = parse("https://www.google.com/finance/markets/cryptocurrencies")
+
     stock_index_name = [x.text for x in \
-                            data.find_all("div",{"class", "ZvmM7"})]
+                            stock_data.find_all("div",{"class", "ZvmM7"})]
 
     stock_index_price = [x.text for x in \
-                            data.find_all("span",{"class","JLPHhb"})]
-    """               
-    stock_index_dollar_change = [x.text for x in \
-                                     data.find_all("span",{"class","P2Luy"})]
-    
-    stock_index_percent_change = [x.text for x in \
-                                     data.find_all("div",{"class","JwB6zf"})]
-    """
+                            stock_data.find_all("span",{"class","JLPHhb"})]
+
+    crypto_name = [x.text for x in \
+                            crypto_data.find_all("div",{"class", "ZvmM7"})]
+
+    crypto_price = [x.text for x in \
+                            crypto_data.find_all("span",{"class","JLPHhb"})]
 
     data_dict = {
-            'name' : stock_index_name[:6],
-            'price' : stock_index_price[:6],
-             #'dollar_change' : stock_index_dollar_change[:6],
-             #'percent_change' : stock_index_percent_change[:6]
+            'stock_name' : stock_index_name[:6],
+            'stock_price' : stock_index_price[:6],
+            'crypto_name' : crypto_name[:6],
+            'crypto_price' : crypto_price[:6],
     }
 
-    return data_dict
+    return json.dumps(data_dict)
 
 app = Flask(__name__)
 app.secret_key = "password"
 
+@app.route("/",methods=['GET','POST'])
+def retrurn_index():
+    return render_template("stock_index.html")
+
+
 #GET data sent on page refresh
-@app.route("/",methods=['GET'])
-def return_scraped_data():
-    print("scraped data sent")
-    
-    #need to scrape here for constant calls!
-    stock_data = parse("https://www.google.com/finance/markets/indexes") 
-    crypto_data = parse("https://www.google.com/finance/markets/cryptocurrencies")
+@app.route("/ReccuringData",methods=['GET','POST'])
+def return_scraped_data():    
+    data = scraped_parsed_data()
+    print(f"dict data: {data}")
+    return data
 
-    stock_dict = scraped_parsed_data(stock_data)
-    crypto_dict = scraped_parsed_data(crypto_data)
-
-    return render_template("stock_index.html", stock_dict=stock_dict,
-                                                crypto_dict=crypto_dict)
 
 def main():
     app.run(debug=True, port="8080")
