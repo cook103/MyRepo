744c68361aef1389cdc9eb66671c6ff8d2a8e6a1
Fernando <alvarad31f@yahoo.com>
2023-07-18 00:13:28 -0700

    Add: call find_best_move in receive_square

--- FullStackApps/TicTacToe/tic_tac_toe.py
+++ FullStackApps/TicTacToe/tic_tac_toe.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, jsonify, request
-import requests
+import random
 
 app = Flask(__name__)
 
 g_matrix = [
-     "", "", "",
-     "", "", "",
-     "", "", "",
+     "_", "_", "_",
+     "_", "_", "_",
+     "_", "_", "_",
 ]
 
 matrix_clicked = {"num": ""}
@@ -24,7 +24,9 @@ def recieve_square():
     """"Recieve the X's spot from the user"""
     data = request.get_json()
     matrix_clicked["num"] = data["button"]
-    
+    if matrix_clicked["num"] != str(0):
+        g_matrix[0] = "X" #temporary test case
+        find_best_move(g_matrix)
     if (matrix_clicked["num"]) == "3":
         print("success")
 
@@ -41,21 +43,22 @@ def get_square(data):
     return jsonify(get_data)
 
 def find_best_move(board):
-    empty = empty_spaces(board)
-    return
+    empty_list = empty_spaces(board)
+    print(empty_list)
+    print(g_matrix)
+    get_square(random.choice(empty_list))
 
 def empty_spaces(board):
-    empty = []
+    empty_list = []
     i = 0
     for space in board:
-        if space == "":
-            empty.append(i)
+        if space == "_":
+            empty_list.append(i)
         i += 1
+    return empty_list
 
 def main(): 
     app.run(debug=True, port="8080")
-    analyze_move()
-    
 
 if __name__ == "__main__":
     main()
