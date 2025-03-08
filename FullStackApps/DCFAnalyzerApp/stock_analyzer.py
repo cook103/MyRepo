d1cac6446ff9b66b38e66ca546fda0171367d36d
mcookjr <cook103@cougars.csusm.edu>
2025-03-08 15:17:46 -0800

    Add dcfAnalyzer webapp

---
+++ FullStackApps/DCFAnalyzerApp/stock_analyzer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# import requests
+import sys
+import os
+from flask import Flask, render_template, json, request
+
+# add dcf path & tool
+sys.path.append(os.path.abspath("../../MyApps/StockAnalysis"))
+try:
+    from DCFAnalyzer.dcf_analyzer import DCFModel
+except Exception as e:
+    print(f"Failed to import DCFModel, error {e}.")
+    sys.exit(1)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def return_index():
+    """Onload data is sent here"""
+    reply_message = {}
+    if request.method == "POST":
+        if request.form.get("ticker") and request.form.get("rate"):
+            ticker = request.form.get("ticker")
+            rate =request.form.get("rate")
+
+            if ticker.isdigit():
+                reply_message = {"error": "ticker cannot be a number."}
+                return render_template("dcf.html", reply_message=reply_message)
+            if not rate.isdigit():
+                reply_message = {"error": "growth rate must be a number."}
+                return render_template("dcf.html", reply_message=reply_message)
+
+            try:
+                reply_message = DCFModel(ticker, float(rate)).run_model()
+            except Exception as e:
+                reply_message = {"error": e}
+        else:
+            reply_message= {"error": "Please fill in all required fields."}
+
+        return render_template("dcf.html", reply_message=reply_message)
+            
+def main():
+    app.run(debug=True, host="0.0.0.0", port="8081")
+
+if __name__ == "__main__":
+    main()
